155d6472c49a743a152c5a370545fa789eca84a3
Su Anli <su.anli@anlitz.com>
2022-01-23 22:00:05 +0800

    chore(nx): setup lint with standardjs

--- apps/template-app/src/app/app.spec.tsx
+++ apps/template-app/src/app/app.spec.tsx
@@ -1,8 +1,8 @@
-import { render } from '@testing-library/react-native';
-import React from 'react';
-import App from './app';
+import { render } from '@testing-library/react-native'
+import React from 'react'
+import App from './app'
 
 test('renders correctly', () => {
-  const { getByText } = render(<App />);
-  expect(getByText('Welcome')).toBeDefined();
-});
+  const { getByText } = render(<App />)
+  expect(getByText('Welcome')).toBeDefined()
+})
--- apps/template-app/src/app/app.tsx
+++ apps/template-app/src/app/app.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import React, { FC } from 'react'
+import { SafeAreaView, Text } from 'react-native'
 
-const App = () => {
+const App: FC = () => {
   return (
     <SafeAreaView>
       <Text>Welcome</Text>
     </SafeAreaView>
-  );
-};
+  )
+}
 
-export default App;
+export default App
