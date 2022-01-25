8ce23538ddded68303835c6cb02c51201c7f72c3
Su Anli <anli@users.noreply.github.com>
2022-01-26 01:10:30 +0800

    chore(shared/ui): create ui libs (#12)

--- apps/storybook-app/src/.storybook/preview.js
+++ apps/storybook-app/src/.storybook/preview.js
@@ -1,10 +1,20 @@
-import {withBackgrounds} from '@storybook/addon-ondevice-backgrounds';
+import { withBackgrounds } from '@storybook/addon-ondevice-backgrounds'
+import { ThemeProvider } from '@nx-react-native/shared/ui'
+import React from 'react'
 
-export const decorators = [withBackgrounds];
+export const decorators = [
+  withBackgrounds,
+  (Story) => (
+    <ThemeProvider>
+      <Story />
+    </ThemeProvider>
+  )
+]
+
 export const parameters = {
   backgrounds: [
-    {name: 'plain', value: 'white', default: true},
-    {name: 'warm', value: 'hotpink'},
-    {name: 'cool', value: 'deepskyblue'},
-  ],
-};
+    { name: 'plain', value: 'white', default: true },
+    { name: 'warm', value: 'hotpink' },
+    { name: 'cool', value: 'deepskyblue' }
+  ]
+}
